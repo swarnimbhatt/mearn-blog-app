cf6fa5ab32de82e4da17db999429ef375433d3cd
swarnimbhatt <swarnimlearningcurve@gmail.com>
2024-09-01 15:15:03 +0530

    Established and tested backend/frontend connection

--- api/index.js
+++ api/index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+
 app.get( "/", (req, resp) => {
     resp.json("test ok");
+});
+
+app.post( "/login", (req, resp) => {
+    const {username, password} = req.body;
+    resp.json({requestData: {username, password} });
+});
+
+app.post( "/register", (req, resp) => {
+    const {username, password} = req.body;
+    resp.json({requestData: {username, password} });
 })
 
 app.listen(4000);
--- client/mearn-blog-ui/src/pages/RegisterPage.js
+++ client/mearn-blog-ui/src/pages/RegisterPage.js
@@ -1,8 +1,22 @@
+import {useState} from 'react';
+
 export default function Register() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function registerUser(e){
+        e.preventDefault();
+        await fetch("http://localhost:4000/register", {
+            method: "POST",
+            body: JSON.stringify({username, password}),
+            headers: {"Content-Type": "application/json"}
+        })
+    }
+
     return (
-        <form action="">
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
+        <form onSubmit={registerUser}>
+            <input type="text" placeholder="username" value={username} onChange={e => setUsername(e.target.value)} />
+            <input type="password" placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
             <button>register</button>
         </form>
     );
