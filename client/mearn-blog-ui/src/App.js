6b8c21b39e3888620d6c28443524717660b02936
swarnimbhatt <swarnimlearningcurve@gmail.com>
2024-09-01 13:34:09 +0530

    Created basic pages for home, login, register

--- client/mearn-blog-ui/src/App.js
+++ client/mearn-blog-ui/src/App.js
@@ -1,23 +1,25 @@
-import './App.css';
-
 import { Routes, Route } from 'react-router-dom';
+
+import './App.css';
 
 import Header from './Header';
 import Post from './Post';
 import Layout from './Layout';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import Register from './pages/RegisterPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Post />} />
 
-        <Route path={'/login'} element={<div>Login page</div>} />
+        <Route index element={<HomePage />} />
 
-        <Route path={'/register'} element={<div>Register page</div>} />
+        <Route path={'/login'} element={<LoginPage />} />
+
+        <Route path={'/register'} element={<Register />} />
       </Route>
-
-
 
     </Routes>
 
---
+++ client/mearn-blog-ui/src/pages/RegisterPage.js
@@ -0,0 +1,9 @@
+export default function Register() {
+    return (
+        <form action="">
+            <input type="text" placeholder="username" />
+            <input type="password" placeholder="password" />
+            <button>register</button>
+        </form>
+    );
+}
