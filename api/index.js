419c3603cd06dfd0e2efc3fdabd1d3e87d2fcf05
swarnimbhatt <swarnimlearningcurve@gmail.com>
2024-09-02 22:11:16 +0530

    Added password encryption and login

--- api/index.js
+++ api/index.js
@@ -9,25 +9,38 @@ app.use(express.json());
 
 const User = require("./models/User");
 
-const username = "XXXXXXXXXXXXXXX";
-const password = "XXXXXXXXXXXXXXX";
-const cluster = "XXXXXXXXXXXXXXX";
-const mongoConnString = `mongodb+srv://${username}:${password}@cluster0.68qmd.mongodb.net/?retryWrites=true&w=majority&appName=${cluster}`;
+const bcrypt = require("bcryptjs");
+const salt = bcrypt.genSaltSync();
+
+const mongoUsername = "XXXXXXXXXXXXXXX";
+const mongoPassword = "XXXXXXXXXXXXXXX";
+const mongoCluster = "XXXXXXXXXXXXXXX";
+const mongoConnString = `mongodb+srv://${mongoUsername}:${mongoPassword}@cluster0.68qmd.mongodb.net/?retryWrites=true&w=majority&appName=${mongoCluster}`;
 mongoose.connect(mongoConnString);
 
 app.get("/", (req, resp) => {
     resp.json("test ok");
 });
 
-app.post("/login", (req, resp) => {
+app.post("/login", async (req, resp) => {
     const { username, password } = req.body;
-    resp.json({ requestData: { username, password } });
+    const userDoc = await User.findOne({username});
+    const isPasswordVerified = bcrypt.compareSync(password, userDoc.password);
+    if(isPasswordVerified){
+        resp.status(200).json("Correct credentials.")
+    }
+    else{
+        resp.status(400).json("Wrong credentials.")
+    }
 });
 
 app.post("/register", async (req, resp) => {
     const { username, password } = req.body;
     try {
-        const userDoc = await User.create({ username, password });
+        const userDoc = await User.create({
+            username,
+            password: bcrypt.hashSync(password, salt)
+        });
         resp.json(userDoc);
     }
     catch (e) {
--- client/mern-blog-ui/src/pages/LoginPage.js
+++ client/mern-blog-ui/src/pages/LoginPage.js
@@ -1,8 +1,28 @@
+import {useState} from 'react';
+
 export default function LoginPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function loginUser(e) {
+        e.preventDefault();
+        const resp = await fetch("http://localhost:4000/login", {
+            method: "POST",
+            body: JSON.stringify({ username, password }),
+            headers: { "Content-Type": "application/json" }
+        });
+        if (resp.status == 200) {
+            alert("Login successfull.")
+        }
+        else {
+            alert("Login failed. Please try again later.")
+        }
+    }
+
     return (
-        <form action="">
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
+        <form onSubmit={loginUser}>
+             <input type="text" placeholder="username" value={username} onChange={e => setUsername(e.target.value)} />
+             <input type="password" placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
             <button>login</button>
         </form>
     );
