5dcca1b33749266d435d284b3882d0d0bdfda1e6
swarnimbhatt <swarnimlearningcurve@gmail.com>
2024-09-02 21:40:41 +0530

    Established conn with mongodb & added user registration

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose");
+
 const express = require("express");
 const app = express();
 
@@ -5,20 +7,33 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
+const User = require("./models/User");
 
-app.get( "/", (req, resp) => {
+const username = "XXXXXXXXXXXXXXX";
+const password = "XXXXXXXXXXXXXXX";
+const cluster = "XXXXXXXXXXXXXXX";
+const mongoConnString = `mongodb+srv://${username}:${password}@cluster0.68qmd.mongodb.net/?retryWrites=true&w=majority&appName=${cluster}`;
+mongoose.connect(mongoConnString);
+
+app.get("/", (req, resp) => {
     resp.json("test ok");
 });
 
-app.post( "/login", (req, resp) => {
-    const {username, password} = req.body;
-    resp.json({requestData: {username, password} });
+app.post("/login", (req, resp) => {
+    const { username, password } = req.body;
+    resp.json({ requestData: { username, password } });
 });
 
-app.post( "/register", (req, resp) => {
-    const {username, password} = req.body;
-    resp.json({requestData: {username, password} });
-})
+app.post("/register", async (req, resp) => {
+    const { username, password } = req.body;
+    try {
+        const userDoc = await User.create({ username, password });
+        resp.json(userDoc);
+    }
+    catch (e) {
+        resp.status(400).json(e);
+    }
+});
 
 app.listen(4000);
 
--- client/mern-blog-ui/src/pages/RegisterPage.js
+++ client/mern-blog-ui/src/pages/RegisterPage.js
@@ -1,16 +1,22 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 export default function Register() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    async function registerUser(e){
+    async function registerUser(e) {
         e.preventDefault();
-        await fetch("http://localhost:4000/register", {
+        const resp = await fetch("http://localhost:4000/register", {
             method: "POST",
-            body: JSON.stringify({username, password}),
-            headers: {"Content-Type": "application/json"}
-        })
+            body: JSON.stringify({ username, password }),
+            headers: { "Content-Type": "application/json" }
+        });
+        if (resp.status == 200) {
+            alert("Registration successfull.")
+        }
+        else {
+            alert("Registration failed. Please try again later.")
+        }
     }
 
     return (
