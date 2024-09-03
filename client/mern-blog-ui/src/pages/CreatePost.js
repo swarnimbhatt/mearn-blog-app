cf0173c10bf4a4001b8c15b8f8d405cc11510f2e
swarnimbhatt <swarnimlearningcurve@gmail.com>
2024-09-03 20:42:49 +0530

    Added form validation and InlineError component integration

--- client/mern-blog-ui/src/pages/CreatePost.js
+++ client/mern-blog-ui/src/pages/CreatePost.js
@@ -2,18 +2,25 @@ import { useEffect, useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css'
 import { Navigate, useParams } from 'react-router-dom';
+import InlineError from '../InlineError';
 
 export default function CreatePost() {
     const [title, setTitle] = useState("");
     const [summary, setSummary] = useState("");
     const [content, setContent] = useState("");
-    const [files, setFile] = useState("");
+    const [thumbnail, setFile] = useState(null);
+    const [errorMsgMap, setErrorMsgMap] = useState({
+        title: false,
+        summary: false,
+        content: false,
+        thumbnail: false,
+    });
 
     const [redirect, setRedirect] = useState(false);
 
     const { id } = useParams();
-    useEffect( () => {
-        if(id){
+    useEffect(() => {
+        if (id) {
             fetch(`http://localhost:4000/post/${id}`, {
                 method: "GET",
                 credentials: "include",
@@ -28,17 +35,30 @@ export default function CreatePost() {
                 });
         }
     }, []);
-
+    function formValidation() {
+        const errorStates = {
+            title: !(title && title.trim()),
+            summary: !(summary && summary.trim()),
+            content: !(content && content.trim()),
+            thumbnail: !(thumbnail)
+        };
+        setErrorMsgMap(errorStates);
+        return !Object.values(errorStates).includes(true);
+    }
     async function createNewPost(e) {
+        e.preventDefault();
+        if (!formValidation())
+            return;
+
         const data = new FormData();
         data.set("title", title);
         data.set("summary", summary);
         data.set("content", content);
-        data.set("file", files[0]);
+        data.set("file", thumbnail[0]);
 
-        e.preventDefault();
+
         const resp = await fetch("http://localhost:4000/post", {
-            method: "POST",
+            method: id ? "PUT" : "POST",
             body: data,
             credentials: "include",
         });
@@ -57,11 +77,61 @@ export default function CreatePost() {
         <>
             <div>Create your new post</div>
             <form onSubmit={createNewPost}>
-                <input type="text" placeholder="title" value={title} onChange={e => setTitle(e.target.value)} />
-                <input type="text" placeholder="summary" value={summary} onChange={e => setSummary(e.target.value)} />
-                <input type="file" onChange={e => setFile(e.target.files)} />
-                <ReactQuill value={content} onChange={newValue => setContent(newValue)} />
-                <button>Create post</button>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Title"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <InlineError
+                        group="FORM_VALIDATION"
+                        errorType="REQUIRED_FIELD"
+                        active={errorMsgMap.title}
+                    />
+                </div>
+
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Summary"
+                        value={summary}
+                        onChange={e => setSummary(e.target.value)}
+                    />
+                    <InlineError
+                        group="FORM_VALIDATION"
+                        errorType="REQUIRED_FIELD"
+                        active={errorMsgMap.summary}
+                    />
+                </div>
+
+                <div>
+                    <input
+                        type="file"
+                        onChange={e => setFile(e.target.files)}
+                    />
+                    <InlineError
+                        group="FORM_VALIDATION"
+                        errorType="REQUIRED_FIELD"
+                        active={errorMsgMap.thumbnail}
+                    />
+                </div>
+
+                <div>
+                    <ReactQuill
+                        value={content}
+                        onChange={setContent}
+                    />
+                    <InlineError
+                        group="FORM_VALIDATION"
+                        errorType="REQUIRED_FIELD"
+                        active={errorMsgMap.content}
+                    />
+                </div>
+
+                <button type="submit">
+                    {id ? "Update post" : "Create post"}
+                </button>
             </form>
         </>
     );
