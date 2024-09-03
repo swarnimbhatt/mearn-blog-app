4c3fcc4d8990d7d5e899b27c6ee55e2cd5247a81
swarnimbhatt <swarnimlearningcurve@gmail.com>
2024-09-03 20:16:03 +0530

    Created inline error component

---
+++ client/mern-blog-ui/src/InlineError.js
@@ -0,0 +1,12 @@
+import { MESSAGES } from './MsgConstants';
+export default function InlineError({group, errorType, PrefixMsg, SuffixMsg, active}) {
+    if(!active)
+        return '';
+    return (
+       <div>
+        {PrefixMsg}
+        {MESSAGES[group][errorType]}
+        {SuffixMsg}
+       </div>
+    )
+}
---
+++ client/mern-blog-ui/src/MsgConstants.js
@@ -0,0 +1,16 @@
+export const MESSAGES = {
+    FORM_VALIDATION: {
+      REQUIRED_FIELD: "This field is required.",
+      INVALID_EMAIL: "Please enter a valid email address.",
+      PASSWORD_TOO_SHORT: "Password must be at least 8 characters long.",
+    },
+    AUTH: {
+      LOGIN_SUCCESS: "Login successful!",
+      LOGIN_FAILURE: "Login failed. Please check your credentials.",
+      LOGOUT_SUCCESS: "You have been logged out.",
+    },
+    GENERIC: {
+      SERVER_ERROR: "An error occurred. Please try again later.",
+      NOT_FOUND: "The requested resource was not found.",
+    }
+  };
