be9e000e5c26ec37bd7029bfd4f42078995cb85e
swarnimbhatt <swarnimlearningcurve@gmail.com>
2024-09-05 23:23:37 +0530

    Used theme variables across

--- client/mern-blog-ui/src/components/GlobalSearch.js
+++ client/mern-blog-ui/src/components/GlobalSearch.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import './styles/GlobalSearch.css';
 
-const GlobalSearch = ({ placeholder = "Search...", onSearch }) => {
+const GlobalSearch = () => {
+    const [query, setQuery] = useState('');
+
+    const handleChange = (event) => {
+        setQuery(event.target.value);
+    };
+
+    const handleSearch = () => {
+        console.log('Searching for:', query);
+    };
+
     return (
-        <div className="global-search-container">
+        <div className="search-box">
             <input
                 type="text"
-                placeholder={placeholder}
-                className="global-search-input"
-                onKeyDown={e => {
-                    if (e.key === 'Enter') {
-                        onSearch(e.target.value);
+                placeholder="Search..."
+                value={query}
+                onChange={handleChange}
+                onKeyPress={(event) => {
+                    if (event.key === 'Enter') {
+                        handleSearch();
                     }
                 }}
             />
-            <button
-                className="global-search-button"
-                onClick={() => onSearch(document.querySelector('.global-search-input').value)}
-            >
-                <FaSearch className="icon" />
-            </button>
+            <FaSearch className="search-icon" onClick={handleSearch} />
         </div>
     );
 };
