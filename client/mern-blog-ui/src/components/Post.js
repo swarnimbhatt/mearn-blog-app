5985025c3fe1924e9ca245fedc90c8e36447c2f9
swarnimbhatt <swarnimlearningcurve@gmail.com>
2024-09-06 17:48:00 +0530

    Styling changes on post cards

--- client/mern-blog-ui/src/components/Post.js
+++ client/mern-blog-ui/src/components/Post.js
@@ -1,20 +1,18 @@
 import { Link } from 'react-router-dom';
 import './styles/Post.css';
-
-export default function Post({ _id, title, summary, cover, content, author, createdAt }) {
+export default function Post({ _id, title, summary, cover, content, author, createdAt, category }) {
     return (
         <Link to={`/post/${_id}`} className="card-container">
             <div className="card-image">
                 <img src={"http://localhost:4000/" + cover} alt={title} />
-                <div className="card-info">
-                    <div className="author-date">
+                <div className="card-hover-info">
+                    <div className="author-info">
                         <span>{author.username}</span>
-                        <span>, </span>
-                        <span>{new Date(createdAt).toDateString()}</span>
+                        <span className="post-date">{new Date(createdAt).toDateString()}</span>
                     </div>
-                    {/* <div className="card-category">
+                    <div className="card-category">
                         <span>{category}</span>
-                    </div> */}
+                    </div>
                 </div>
             </div>
             <div className="card-content">
